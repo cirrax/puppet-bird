8e2438ded951bd6f0a2755328289566ba4736ffd
Benedikt Trefzer <benedikt.trefzer@cirrax.com>
2025-07-23 18:07:22 +0200

    let fix_lint do it's job

--- types/bgp/neighbor.pp
+++ types/bgp/neighbor.pp
@@ -1,8 +1,8 @@
 # bgp neighbor definition
 type Bird::Bgp::Neighbor = Struct[{
-  'ip'     => Optional[String[1]],
-  'prefix' => Optional[String[1]],
-  'port'   => Optional[Integer],
-  'as'     => Optional[Integer],
-  'type'   => Optional[Enum['internal','external']],
+    'ip'     => Optional[String[1]],
+    'prefix' => Optional[String[1]],
+    'port'   => Optional[Integer],
+    'as'     => Optional[Integer],
+    'type'   => Optional[Enum['internal','external']],
 }]
--- types/define/generic.pp
+++ types/define/generic.pp
@@ -1,7 +1,7 @@
 # parameters for define bird::generic
 type Bird::Define::Generic = Struct[{
-  'filename' => Optional[String[1]],
-  'order'    => Optional[String[1]],
-  'comments' => Optional[Array[String[1]]],
-  'content'  => Optional[String[1]],
+    'filename' => Optional[String[1]],
+    'order'    => Optional[String[1]],
+    'comments' => Optional[Array[String[1]]],
+    'content'  => Optional[String[1]],
 }]
