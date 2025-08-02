f61914e2c277c39e3071454b8e5eb777e6026994
Benedikt Trefzer <benedikt.trefzer@cirrax.com>
2025-08-02 21:49:17 +0200

    switch to our skeleton

--- types/table.pp
+++ types/table.pp
@@ -1,11 +1,11 @@
 # table config
 type Bird::Table = Struct[{
-  'nettype'         => Enum['ipv4', 'ipv4 sadr', 'ipv6', 'ipv6 sadr', 'vpn4', 'vpn6', 'roa4', 'roa6', 'flow4', 'flow6', 'mpls'], 
-  'debug'           => Optional[Enum['all','off']],
-  'sorted'          => Optional[Boolean],
-  'trie'            => Optional[Boolean],
-  'min_settle_time' => Optional[String[1]],
-  'max_settle_time' => Optional[String[1]],
-  'gc_threshold'    => Optional[Integer],
-  'gc_period'       => Optional[String[1]],
+    'nettype'         => Enum['ipv4', 'ipv4 sadr', 'ipv6', 'ipv6 sadr', 'vpn4', 'vpn6', 'roa4', 'roa6', 'flow4', 'flow6', 'mpls'],
+    'debug'           => Optional[Enum['all','off']],
+    'sorted'          => Optional[Boolean],
+    'trie'            => Optional[Boolean],
+    'min_settle_time' => Optional[String[1]],
+    'max_settle_time' => Optional[String[1]],
+    'gc_threshold'    => Optional[Integer],
+    'gc_period'       => Optional[String[1]],
 }]
